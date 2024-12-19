6bbed1a0a09892f4a0233706ac33d110a4b189cd
Harkirat Singh <harkirat.kirat0602@gmail.com>
2024-12-19 21:59:12 +0530

    before changing code

--- Arduino_Sketch/sketch_dec9a/sketch_dec9a.ino
+++ Arduino_Sketch/sketch_dec9a/sketch_dec9a.ino
@@ -33,6 +33,27 @@ char* IDLE_TEXT = "IDLE Mode";
 char* IDLE_TEXT2 = "Scan Your Card";
 
 
+MFRC522::MIFARE_Key key;
+key.keyByte = { 0xA6, 0xB5, 0xC4, 0xD3, 0xE2, 0xF1 };
+
+
+void printlcd(char* message,byte row=0,byte clear=1){
+  if(clear){
+    lcd.clear();
+  }
+  lcd.setCursor(0,row);
+  lcd.print(message);
+}
+
+
+void showError(char* message){
+  printlcd("[X] "+message);
+  MODE = "idle";
+}
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
   
@@ -41,34 +62,27 @@ void setup() {
 
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0,0);
-  lcd.print("Welcome");
+  printlcd("Welcome");
   delay(500);
 
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Connecting to WiFi");
+  printlcd("Connecting to WiFi")l
 
   WiFi.begin(WiFi_SSID,WiFi_Pass);
   while(WiFi.status() != WL_CONNECTED){
     lcd.print(".");
     delay(500);
   }
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("WiFi Connected!!");
+  printlcd("WiFi Connected!!");
   delay(500);
-  lcd.clear();
-  lcd.setCursor(0,0);
+
 
   client.setServer(MQTT_IP,MQTT_User,MQTT_Pass);
   client.setCallback(callback);
 
-  lcd.print("Connecting to MQTT Broker");
+  printlcd("Connecting to MQTT Broker");
   while(!client.connected()){
     if(client.connect("ArduinoClient")){
-      lcd.setCursor(0,1);
-      lcd.print("Connected!!");
+      printlcd("Connected!!",1,0);
       client.subscribe("attendance/command");
       client.subscribe("attendance/data");
     }else{
@@ -84,6 +98,8 @@ void setup() {
   pinMode(BUZZER_PIN,OUTPUT);
 
 }
+
+
 
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -109,15 +125,79 @@ void callback(char* topic, byte* payload, unsigned int length) {
     DeserializationError error = deserializeJson(doc,message);
 
     if(error){
-      lcd.setCursor(0,0);
-      lcd.print("JSON Parse Error");
-      MODE = "idle";
+      showError("JSON Parse Error");
       return;
     }
 
     //write the logic to write
 
+    printlcd("Ready to write Card");
 
+    MFRC522::MIFARE_Key newkey;
+    for(byte i=0; i<6; i++) newkey.keyByte[i] = 0xFF;
+
+
+    while(!mfrc522.PICC_IsNewCardPresent()){
+      delay(100);
+    }
+
+    if(!mfrc522.PICC_ReadCardSerial()){
+      showError("Card Read Error");
+      return;
+    }
+
+    MFRC522::StatusCode status;
+    byte block;
+    block=1;
+
+    status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &newkey, &(mfrc522.uid));
+
+    if(status!= MFRC::STATUS_OK){
+      showError("Authentication Error")
+      return;
+    }
+
+    status = mfrc522.MIFARE_Write(block, (byte*)doc["firstname"], strlen(doc["firstname"]));
+
+    if(status!= MFRC::STATUS_OK){
+      showError("Error while writing")
+      return;
+    }
+
+    block++;
+
+    status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &newkey, &(mfrc522.uid));
+
+    if(status!= MFRC::STATUS_OK){
+      showError("Authentication Error")
+      return;
+    }
+
+    status = mfrc522.MIFARE_Write(block, (byte*)doc["lastname"], strlen(doc["lastname"]));
+
+    if(status!= MFRC::STATUS_OK){
+      showError("Error while writing")
+      return;
+    }
+
+
+    block++;
+
+    status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &newkey, &(mfrc522.uid));
+
+    if(status!= MFRC::STATUS_OK){
+      showError("Authentication Error")
+      return;
+    }
+
+    status = mfrc522.MIFARE_Write(block, (byte*)doc["rollno"], strlen(doc["rollno"]));
+
+    if(status!= MFRC::STATUS_OK){
+      showError("Error while writing")
+      return;
+    }
+
+    printlcd("Card written successfully!!  ");
 
   }
 
@@ -127,5 +207,47 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  client.loop();
+
+  if(!mfrc522.IsNewCardPresent()){
+    return;
+  }
+
+  if(!mfrc522.PICC_ReadCardSerial()){
+    showError("Card Read Error");
+    return;
+  }
+
+
+  MFRC522::StatusCode status;
+  byte block;
+  block=1;
+  byte buffer[18];
+
+
+  if(strcmp(MODE,"read")==0){ 
+    DynamicJsonDocument doc(200);
+
+    status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
+
+    if(status!= MFRC::STATUS_OK){
+      showError("Authentication Error")
+      return;
+    }
+
+    status = mfrc522.MIFARE_Read(block, buffer, 16);
+
+    if(status!= MFRC::STATUS_OK){
+      showError("Error while writing")
+      return;
+    }
+    strcpy(doc["firstname"],buffer);
+
+    buffer=0;
+
+
+  }
+
 
 }
