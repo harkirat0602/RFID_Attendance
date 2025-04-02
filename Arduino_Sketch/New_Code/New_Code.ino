0206a9c9a4621f581ccb07c9f06ae11774777f86
Harkirat Singh <harkirat.kirat0602@gmail.com>
2025-04-02 14:18:55 +0530

    Arduino Working RFID with MQTT and LCD

--- Arduino_Sketch/New_Code/New_Code.ino
+++ Arduino_Sketch/New_Code/New_Code.ino
@@ -2,6 +2,7 @@
 #include <PubSubClient.h>
 #include <MFRC522.h>
 #include <ESP8266WiFi.h>
+#include <LiquidCrystal_PCF8574.h>
 
 
 #define RST_PIN         0 
@@ -13,26 +14,46 @@ PubSubClient client(espClient);
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
+LiquidCrystal_PCF8574 lcd(0x27);
+
+
+void printlcd(const char* message,byte row=0,byte clear=1){
+  static char lasttext[] ="";
+  if(strcmp(lasttext,message)==0) return;
+  if(clear){
+    lcd.clear();
+  }
+  lcd.setCursor(0,row);
+  lcd.print(message);
+  Serial.println(message);
+  // strcpy(lasttext,message);
+}
+
+
+
 
 void setup() {
   // put your setup code here, to run once:
   SPI.begin();
   mfrc522.PCD_Init();
 
+  lcd.begin(16, 2);  // Initialize 16x2 LCD
+  lcd.setBacklight(255);
+
   Serial.begin(9600);
 
-  Serial.println("Connecting to WiFi");
+  printlcd("Connecting to WiFi");
 
   WiFi.begin("Kirat","11101970Pa");
   while(WiFi.status() != WL_CONNECTED){
     delay(200);
   }
-  Serial.println("WiFi Connected");
+  printlcd("WiFi Connected");
 
 
   client.setServer("192.168.29.241",1883);
 
-  Serial.println("Connecting to MQTT Broker");
+  printlcd("Connecting to MQTT Broker");
   short int attempts=0;
   while(!client.connected() && attempts<5){
     attempts++;
@@ -53,24 +74,22 @@ void loop() {
   if(!mfrc522.PICC_IsNewCardPresent()){
     return;
   }
-  Serial.println("Got New Card");
+  printlcd("Got New Card");
 
   if(!mfrc522.PICC_ReadCardSerial()){
-    Serial.println("Card Read Error");
+    printlcd("Card Read Error");
     return;
   }
 
   String UID ="";
 
-  Serial.print("Card UID:");
+  printlcd("Card UID:");
   for (byte i = 0; i < mfrc522.uid.size; i++) {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
     UID.concat(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
     UID.concat(mfrc522.uid.uidByte[i]);
   }
-  Serial.println();
   client.publish("sample",UID.c_str());
+  printlcd(UID.c_str());
 
   // Halt PICC
   mfrc522.PICC_HaltA();
